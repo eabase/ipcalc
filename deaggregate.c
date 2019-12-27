2888293282c03269c1f9dac78ead7756dedeb65a
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2019-12-27 06:37:24 +0100

    Change licenses to GPLv2 or later to code written by me
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@gnutls.org>

--- deaggregate.c
+++ deaggregate.c
@@ -2,8 +2,9 @@
  * Copyright (c) 2019 Nikos Mavrogiannopoulos
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License, version 2,
- * as published by the Free Software Foundation.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -12,7 +13,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #include <stdio.h>
